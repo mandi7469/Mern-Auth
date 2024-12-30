46842257b7b87ceb17d5ca37e74ee3118d441567
mandi7469 <mandi7469@aol.com>
2024-12-30 16:53:54 -0500

    created user dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login";
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
 import { UserContextProvider } from "../context/userContext";
+import Dashboard from "./pages/Dashboard";
 
 // configure axios defaults for all requests
 axios.defaults.baseURL = "http://localhost:8000";
@@ -23,6 +24,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </UserContextProvider>
     </>
